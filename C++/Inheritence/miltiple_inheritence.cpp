0a1f4bf58360bd332528202d93137daff6efa748
Farhan-Mr <farhat.hp2022@gmail.com>
2025-09-16 20:34:24 +0530

    Multiple Inheritence eg.

--- C++/Inheritence/miltiple_inheritence.cpp
+++ C++/Inheritence/miltiple_inheritence.cpp
@@ -1,5 +1,28 @@
 /* Program of multiple inheritence */
 
 #include <iostream>
-    using manespace std:
-    
+    using namespace std;
+
+    class A     // Base class 
+    {
+        protected : int n;
+
+        public :
+
+        void input ()
+        {
+            cout<<"Enter number : ";
+            cin>>n;
+        }
+
+        void display ()
+        {
+            cout<<"Number is -> "<<n;
+        }
+    };
+
+    int main ()
+    {
+        return 0;
+    }
+
