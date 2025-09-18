05b6073cb5f00bfac4a3b5ee20f3d2a69d242b70
Farhan-Mr <farhat.hp2022@gmail.com>
2025-09-18 19:21:31 +0530

    Single Inheritence (Factorial)

---
+++ C++/Inheritence/Single_Inheritence_factorial.cpp
@@ -0,0 +1,42 @@
+/* Program of Single Inheritence to find factorial of a number 
+    and number taken by User */
+
+    #include<iostream>
+        using namespace std;
+
+        class Fact
+        {
+            protected : int n;
+
+            public :
+
+            void input ()
+            {
+                cout <<"Enter Number for Factorial : ";
+                cin>>n;
+            }
+            void show ()
+            {
+                cout << "Number is = "<< n;
+            }
+        };
+
+        class facts : public Fact
+        {
+            protected : int F = 1;
+
+            public :
+            void factorial ()
+            {
+                for (int i=1; i<n; i++)
+                {
+                    F *= i;
+                }
+            }
+
+            void answer ()
+            {
+                cout << "factorial of "<<n<< " is = " << F;
+            }
+        };
+        
