e2c04d8727e411eabdffafe31e6eaabddb66c781
Farhan-Mr <farhat.hp2022@gmail.com>
2025-09-19 17:39:39 +0530

    Single inheritence Question

---
+++ C++/Inheritence/Single_inheritence2.cpp
@@ -0,0 +1,28 @@
+
+/*  1. Define a base class Person with data members for name and age and a method display(). 
+    2. Define a derived class Student that inherits from Person. 
+    3. The Student class should have an addi onal data member for rollNumber and a method 
+        displayStudent() that calls the display() method of the base class and also prints the rollNumber. 
+    4. In the main() func on, create an object of the Student class and demonstrate its funconality. 
+*/
+
+#include<iostream>
+    using namespace std;
+
+    class Person
+    {
+        public :
+        string name;
+        int age ;
+
+        void input()
+        {
+            cout << "Enter name : ";
+            cin >> name;
+
+            cout << "Enter age : ";
+            cin >> age;
+        }
+
+
+    };
